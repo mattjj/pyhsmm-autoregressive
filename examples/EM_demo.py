005265e1ebdb9b06ce8209c2775128d6d1f07dfd
Matthew Johnson <mattjj@csail.mit.edu>
2013-04-09 16:09:06 -0400

    added EM demo which seems to work!

---
+++ examples/EM_demo.py
@@ -0,0 +1,67 @@
+from __future__ import division
+
+import numpy as np
+from matplotlib import pyplot as plt
+plt.ion()
+
+import pyhsmm
+from pyhsmm.util.text import progprint_xrange
+import pyhsmm.plugins.autoregressive.models as m
+import pyhsmm.plugins.autoregressive.distributions as d
+
+###############
+#  make data  #
+###############
+
+a = d.MNIW(3,np.eye(2),np.zeros((2,4)),np.eye(4))
+a.A = np.hstack((-np.eye(2),2*np.eye(2)))
+
+b = d.MNIW(3,np.eye(2),np.zeros((2,4)),np.eye(4))
+b.A = np.array([[np.cos(np.pi/6),-np.sin(np.pi/6)],[np.sin(np.pi/6),np.cos(np.pi/6)]]).dot(np.hstack((-np.eye(2),np.eye(2)))) + np.hstack((np.zeros((2,2)),np.eye(2)))
+
+c = d.MNIW(3,np.eye(2),np.zeros((2,4)),np.eye(4))
+c.A = np.array([[np.cos(-np.pi/6),-np.sin(-np.pi/6)],[np.sin(-np.pi/6),np.cos(-np.pi/6)]]).dot(np.hstack((-np.eye(2),np.eye(2)))) + np.hstack((np.zeros((2,2)),np.eye(2)))
+
+
+data = np.array([0,2]).repeat(2).reshape((2,2))
+distns = [a,b,c]
+for i in range(9):
+    data = np.concatenate((data,distns[i % len(distns)].rvs(prefix=data[-2:],length=30)))
+
+plt.figure()
+plt.plot(data[:,0],data[:,1],'bx-')
+
+####################
+#  make DAT MODEL  #
+####################
+
+Nmax = 20
+model = m.ARHMM(
+        nlags=2,
+        alpha=4.,gamma=4.,init_state_concentration=10.,
+        obs_distns=[d.MNIW(3,np.eye(2),np.zeros((2,4)),np.eye(4)) for state in range(Nmax)],
+        )
+
+model.add_data(data)
+
+######################
+#  do DAT INFERENCE  #
+######################
+
+print 'Gibbs sampling initialization'
+for itr in progprint_xrange(5):
+    model.resample_model()
+
+print 'EM'
+for itr in progprint_xrange(50):
+    model.EM_step()
+
+plt.figure()
+model.plot()
+
+plt.figure()
+colors = ['b','r','y','k','g']
+stateseq = model.states_list[0].stateseq
+for i,s in enumerate(np.unique(stateseq)):
+    plt.plot(data[s==stateseq,0],data[s==stateseq,1],colors[i % len(colors)] + 'o')
+
