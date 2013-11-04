c7df41e8ff13b505afd1047bfb58083f8a1d0ccb
Matthew Johnson <mattjj@csail.mit.edu>
2013-11-04 10:09:05 -0500

    factored out _ARMixin

--- models.py
+++ models.py
@@ -12,21 +12,22 @@ from pyhsmm.util.general import rle
 # these exist only to play appropriate stride tricks on the data
 # and to instantiate the appropriate states classes for forward generation
 
-class ARHMM(pyhsmm.models.HMM):
-    _states_class = ARHMMStates
-
+class _ARMixin(object):
     def __init__(self,nlags,*args,**kwargs):
-        super(ARHMM,self).__init__(*args,**kwargs)
+        super(_ARMixin,self).__init__(*args,**kwargs)
         self.nlags = nlags
 
     def add_data(self,data,**kwargs):
         strided_data = AR_striding(data,self.nlags)
-        self.states_list.append(
-                self._states_class(
-                    nlags=self.nlags,
-                    model=self,
-                    data=strided_data,
-                    **kwargs))
+        super(_ARMixin,self).add_data(
+                data=strided_data,
+                nlags=self.nlags)
+
+    def add_data_parallel(self,data,broadcast=False,**kwargs):
+        strided_data = AR_striding(data,self.nlags)
+        super(_ARMixin,self).add_data_parallel(
+                data=strided_data,
+                nlags=self.nlags)
 
     def plot_observations(self,colors=None,states_objs=None):
         if colors is None:
@@ -52,6 +53,10 @@ class ARHMM(pyhsmm.models.HMM):
         return super(ARHMM,self).generate(*args,nlags=self.nlags,**kwargs)
 
 
+class ARHMM(_ARMixin,pyhsmm.models.HMM):
+    _states_class = ARHMMStates
+
+
 class ARHMMEigen(ARHMM):
     _states_class = ARHMMStatesEigen
 
@@ -64,40 +69,6 @@ class ARStickyHMMEigen(ARHMMEigen,pyhsmm.models.StickyHMMEigen):
         self.nlags = nlags
 
 
-class ARHSMM(pyhsmm.models.HSMM):
+class ARHSMM(_ARMixin,pyhsmm.models.HSMM):
     _states_class = ARHSMMStates
-
-    def __init__(self,nlags,*args,**kwargs):
-        super(ARHSMM,self).__init__(*args,**kwargs)
-        self.nlags = nlags
-
-    def add_data(self,data,**kwargs):
-        strided_data = AR_striding(data,self.nlags)
-        self.states_list.append(
-                ARHSMMStates(
-                    nlags=self.nlags,
-                    model=self,
-                    data=strided_data,
-                    **kwargs))
-
-    # TODO repeated code from ARHMM class
-    def plot_observations(self,colors=None,states_objs=None):
-        if colors is None:
-            colors = self._get_colors()
-        if states_objs is None:
-            states_objs = self.states_list
-
-        cmap = cm.get_cmap()
-
-        for s in states_objs:
-            data = undo_AR_striding(s.data,self.nlags)
-
-            stateseq_norep, durs = rle(s.stateseq)
-            starts = np.concatenate(((0,),durs.cumsum()))
-            for state,start,dur in zip(stateseq_norep,starts,durs):
-                plt.plot(
-                        np.arange(start,start+data[start:start+dur].shape[0]),
-                        data[start:start+dur],
-                        color=cmap(colors[state]))
-            plt.xlim(0,s.T-1)
 
