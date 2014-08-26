43aea72d28034299dffe91db3f061d35212afc99
Matthew Johnson <mattjj@csail.mit.edu>
2014-08-26 12:55:36 -0400

    make only one init emission distn

--- models.py
+++ models.py
@@ -12,28 +12,30 @@ from autoregressive.states import ARHMMStates, ARHSMMStates, \
         ARHMMStatesEigen, ARHSMMStatesEigen, ARHSMMStatesGeo
 
 class _ARMixin(object):
-    def __init__(self,init_emission_distns=None,**kwargs):
+    def __init__(self,init_emission_distn=None,**kwargs):
         super(_ARMixin,self).__init__(**kwargs)
-        if init_emission_distns is None:
-            init_emission_distns = \
-                    [Gaussian(nu_0=self.P+10,sigma_0=np.eye(self.P),
+        if init_emission_distn is None:
+            init_emission_distn = \
+                    Gaussian(nu_0=self.P+10,sigma_0=np.eye(self.P),
                         mu_0=np.zeros(self.P),kappa_0=1.)
-                        for _ in range(self.num_states)]
-        self.init_emission_distns = init_emission_distns
+        self.init_emission_distn = init_emission_distn
 
     def add_data(self,data,strided=False,**kwargs):
         strided_data = AR_striding(data,self.nlags) if not strided else data
         super(_ARMixin,self).add_data(data=strided_data,**kwargs)
+
+    ### Gibbs
 
     def resample_parameters(self):
         super(_ARMixin,self).resample_parameters()
         self.resample_init_emission_distn()
 
     def resample_init_emission_distn(self):
-        for state, d in enumerate(self.init_emission_distns):
-            d.resample([s.data[:self.nlags].ravel()
+        self.init_emission_distn.resample(
+            [s.data[:self.nlags].ravel()
                 for s in self.states_list if s.stateseq[0] == state])
-        self._clear_caches()
+
+    ### convenient properties
 
     @property
     def nlags(self):
@@ -46,6 +48,8 @@ class _ARMixin(object):
     @property
     def P(self):
         return self.D*self.nlags
+
+    ### plotting
 
     def plot_observations(self,colors=None,states_objs=None):
         if colors is None:
