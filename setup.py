82ce5dd21d2906463758c27ab3efbeded93c91e6
Matthew Johnson <mattjj@csail.mit.edu>
2015-05-18 09:21:20 -0400

    fix old and busted setup.py file

--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
 from distutils.core import setup
 import numpy as np
-import os, sys
+import os
+from Cython.Build import cythonize
 
 import pyhsmm
-from pyhsmm.util.cyutil import cythonize # my version of Cython.Build.cythonize
 eigen_include_path = os.path.join(
-        os.path.dirname(pyhsmm.__file__),'deps/Eigen3/')
+    os.path.dirname(pyhsmm.__file__),'deps','Eigen3')
 pyhsmm_include_path = os.path.join(
-        os.path.dirname(pyhsmm.__file__),'internals/')
+    os.path.dirname(pyhsmm.__file__),'internals')
 
 extra_compile_args = ['-w','-DNDEBUG']
 extra_link_args =[]
-
-if '--with-mkl' in sys.argv:
-    sys.argv.remove('--with-mkl')
-    extra_compile_args.extend(['-m64','-I' + os.environ['MKLROOT'] + '/include','-DEIGEN_USE_MKL_ALL'])
-    extra_link_args.extend(('-Wl,--start-group %(MKLROOT)s/lib/intel64/libmkl_intel_lp64.a %(MKLROOT)s/lib/intel64/libmkl_core.a %(MKLROOT)s/lib/intel64/libmkl_sequential.a -Wl,--end-group -lm' % {'MKLROOT':os.environ['MKLROOT']}).split(' '))
 
 ext_modules = cythonize('**/*.pyx')
 for e in ext_modules:
