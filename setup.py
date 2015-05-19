538e6cbb24e67aaa7a0d14839defc75b5036a186
Matthew Johnson <mattjj@csail.mit.edu>
2015-05-19 09:06:24 -0400

    make autoregressive installable (hopefully)

--- setup.py
+++ setup.py
@@ -18,7 +18,21 @@ for e in ext_modules:
     e.extra_link_args.extend(extra_link_args)
 
 setup(
+    name='autoregressive',
+    author='Matthew James Johnson',
+    author_email='mattjj@csail.mit.edu',
+    url='https://github.com/mattjj/pyhsmm-autoregressive',
+    keywords=
+        ['bayesian', 'inference', 'mcmc', 'time-series',
+         'autoregressive', 'var', 'svar'],
+    install_requires=
+        ['Cython >= 0.20.1', 'numpy', 'scipy',
+         'matplotlib', 'pybasicbayes', 'pyhsmm'],
     ext_modules=ext_modules,
     include_dirs=[np.get_include(),eigen_include_path,pyhsmm_include_path],
+    classifiers=
+        ['Intended Audience :: Science/Research',
+         'Programming Language :: Python',
+         'Programming Language :: C++'],
 )
 
