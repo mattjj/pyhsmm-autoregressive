839b548c61132d4092f1c256c248f3767536ee11
Matthew Johnson <mattjj@csail.mit.edu>
2015-09-10 10:24:29 -0400

    improve util organization

--- autoregressive/util.py
+++ autoregressive/util.py
@@ -2,6 +2,7 @@ from __future__ import division
 import numpy as np
 from numpy.lib.stride_tricks import as_strided as ast
 
+
 ### striding data for efficient AR computations
 
 def AR_striding(data,nlags):
@@ -17,6 +18,7 @@ def AR_striding(data,nlags):
             shape=(data.shape[0]-nlags,data.shape[1]*(nlags+1)),
             strides=(data.shape[1]*sz,sz))
 
+
 def undo_AR_striding(strided_data,nlags):
     sz = strided_data.dtype.itemsize
     return ast(
@@ -24,32 +26,12 @@ def undo_AR_striding(strided_data,nlags):
             shape=(strided_data.shape[0]+nlags,strided_data.shape[1]/(nlags+1)),
             strides=(strided_data.shape[1]/(nlags+1)*sz,sz))
 
-### analyzing AR coefficient matrices
 
-def canonical_matrix(A):
-    # NOTE: throws away affine part
-    D, nlags, _ = dimensions(A)
-    mat = np.zeros((D*nlags,D*nlags))
-    mat[:-D,D:] = np.eye(D*(nlags-1))
-    mat[-D:,:] = A[:,:D*nlags]
-    return mat
-
-def eval_siso_transfer_function(A,from_idx,to_idx,freqs):
-    D, _, _ = dimensions(A)
-    assert 0 <= from_idx < D and 0 <= to_idx < D
-    bigA = canonical_matrix(A)
-    I = np.eye(bigA.shape[0])
-    zs = np.exp(1j*np.array(freqs))
-    return np.array(
-            [np.linalg.inv(z*I-bigA)[-D:,-2*D:-D][to_idx,from_idx]
-                for z in zs])
+### data properties of AR coefficient matrices
 
 def is_affine(A):
     return bool(A.shape[1] % A.shape[0])
 
-def is_stable(A):
-    bigA = canonical_matrix(A)
-    return np.all(np.abs(np.linalg.eigvals(bigA)) < 1.)
 
 def dimensions(A):
     if is_affine(A):
@@ -57,3 +39,32 @@ def dimensions(A):
     D, nlags = A.shape[0], A.shape[1] // A.shape[0]
     return D, nlags, is_affine(A)
 
+
+### analyzing AR coefficient matrices
+
+def canonical_matrix(A):
+    assert not is_affine(A), 'this function ignores affine parts of A'
+    D, nlags, _ = dimensions(A)
+    mat = np.zeros((D*nlags,D*nlags))
+    mat[:-D,D:] = np.eye(D*(nlags-1))
+    mat[-D:,:] = A[:,:D*nlags]
+    return mat
+
+
+def siso_transfer_function(A,i,j):
+    D, nlags, _ = dimensions(A)
+    assert 0 <= i < nlags*D and 0 <= j < D
+    bigA = canonical_matrix(A)
+    I = np.eye(bigA.shape[0])
+
+    def H(freqs):
+        zs = np.exp(1j*np.array(freqs))
+        return np.array(
+            [np.linalg.inv(z*I-bigA)[-D:,-2*D:-D][j,i] for z in zs])
+
+    return H
+
+
+def is_stable(A):
+    bigA = canonical_matrix(A)
+    return np.all(np.abs(np.linalg.eigvals(bigA)) < 1.)
