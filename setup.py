7918f0e0116f763354637da0b2f4138a74ae1aa2
Matthew Johnson <mattjj@csail.mit.edu>
2016-01-05 13:54:11 -0500

    make setup.py compile things again, add back messages.pyx

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
 from distutils.core import setup
+from setuptools.command.build_ext import build_ext as _build_ext
+from Cython.Build import cythonize
+import numpy as np
+from os.path import dirname, join
+
+import pyhsmm
+
+pyhsmm_path = dirname(dirname(pyhsmm.__file__))
+eigen_include = join(pyhsmm_path, 'deps')
+pyhsmm_include = join(pyhsmm_path, 'pyhsmm', 'internals')
+
+print eigen_include
 
 setup(
     name='autoregressive',
@@ -23,4 +35,6 @@ setup(
         'Intended Audience :: Science/Research',
         'Programming Language :: Python',
         'Programming Language :: C++'],
+    ext_modules=cythonize('**/*.pyx'),
+    include_dirs=[np.get_include(), eigen_include, pyhsmm_include]
 )
